52357ad7836d75dcac6bf1e31a0fa9a1b05cd135
shivamsince0502 <shubhamkumarsingh7209@gmail.com>
2023-02-26 03:02:38 +0530

    change

--- QA Automation Amazon/cypress/e2e/amazonTest.cy.js
+++ QA Automation Amazon/cypress/e2e/amazonTest.cy.js
@@ -5,14 +5,14 @@ describe('Amazon Login and Product add to cart', () => {
   it('should open Amazon website', () => {
     cy.visit('https://www.amazon.in/')
     cy.get("#nav-link-accountList").click();
-    cy.get('#ap_email').type('thenexus6969@gmail.com')
+    cy.get('#ap_email').type('your mail')
     cy.get('#continue').click()
-    cy.get('#ap_password').type('Hala007@')
+    cy.get('#ap_password').type('your Password')
     cy.get('#signInSubmit').click()
     cy.get('#nav-link-accountList-nav-line-1').should('contain', 'Hello, Shivam')
     cy.get('#twotabsearchtextbox').type('Realme Phone')
     cy.get('#nav-search-submit-button').click();
-    cy.get('[data-image-index="1"]').click() 
+    cy.get('[data-image-index="1"]').click()
     cy.window()
     cy.get('#buy-now-button').click()
     // cy.window().then((win) => {
@@ -21,7 +21,7 @@ describe('Amazon Login and Product add to cart', () => {
     //   // Verify the content on the new tab
     //   cy.title().should('contain', 'Google')
     // })
-  
+
   })
 
 
